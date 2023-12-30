0fdf5e78e3ccf1999b0e5ba7526035dea05cb09f
heeba-devops <131389878+heeba-devops@users.noreply.github.com>
2023-12-30 20:29:26 +0400

    Updated again seventh.py

--- seventh.py
+++ seventh.py
@@ -3,5 +3,5 @@ while True:
   print("please type \"exit\" to terminate the program")
   out=input()
   if out == "exit":
-  sys.exit()
+     sys.exit()
   print("u entered "+out+"!"+"kindly type the right respose")
